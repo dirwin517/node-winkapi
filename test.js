022d92a48977f5709ec9dbecddb125592ab38f4a
Daniel Irwin <daniel.irwin@enernoc.com>
2016-04-03 19:59:23 -0400

    some major overhauls

--- test.js
+++ test.js
@@ -1,48 +1,101 @@
-var WinkAPI = require('./winkapi')
-  , util    = require('util')
-  ;
+var WinkAPI = require('./winkapi');
+var configFile = {};
 
-var clientID     = '...'
-  , clientSecret = '...'
-  , userName     = '...'
-  , passPhrase   = '...'
-  , winkapi
-  ;
+try{
+  configFile = require('./config.json')
+}
+catch(e){
+  console.log('could not load config via config.json, using environment properties instead');
+}
 
-winkapi = new WinkAPI.WinkAPI({ clientID: clientID, clientSecret: clientSecret }).login(userName, passPhrase, function(err) {
-  if (!!err) return console.log('login error: ' + err.message);
+var config = {
+  clientID      : configFile.clientID      || process.env.clientID,
+  clientSecret  : configFile.clientSecret  || process.env.clientSecret,
+  userName      : configFile.userName      || process.env.userName,
+  passPhrase    : configFile.passPhrase    || process.env.passPhrase,
+  //logger:{ }
+};
 
-  winkapi.getUser(function(err, user) {
-    if (!!err) return console.log('getUser: ' + err.message);
+describe('Test Wink API Client', function() {
+  this.timeout(5000);
 
-    console.log('user:'); console.log(user);
-  }).getDevices(function(err, devices) {
-    var i;
+  function setup(callback){
+    var winky = new WinkAPI.WinkAPI(config);
+    winky.login(config.userName, config.passPhrase, callback)
+      .on('error', function(err) {
+      console.log('background error: ' + err.message);
+    });
+  }
 
-    if (!!err) return console.log('getDevices: ' + err.message);
-
-    console.log('devices:');
-    for (i = 0; i < devices.length; i++) {
-      console.log('device #' + i);
-      console.log(util.inspect(devices[i], { depth: null }));
-    }
-  }).getIcons(function(err, icons) {
-    if (!!err) return console.log('getIcons: ' + err.message);
-
-    console.log('icons:'); console.log(icons);
-  }).getChannels(function(err, channels) {
-    if (!!err) return console.log('getChannels: ' + err.message);
-
-    console.log('channels:'); console.log(channels);
-  }).getDialTemplates(function(err, dialTemplates) {
-    if (!!err) return console.log('getDialTemplates: ' + err.message);
-
-    console.log('dialTemplates:'); console.log(dialTemplates);
-  }).getServices(function(err, services) {
-    if (!!err) return console.log('getServices: ' + err.message);
-
-    console.log('services:'); console.log(services);
+  it('login', function(done){
+    setup(function(err){
+      if(err){
+        throw new Error(err);
+      }
+      done();
+    })
   });
-}).on('error', function(err) {
-  console.log('background error: ' + err.message);
+
+  it('get users', function(done){
+    setup(function(err, winkapi) {
+      winkapi.getUser(done);
+    });
+  });
+
+  it('get devices', function(done){
+    setup(function(err, winkapi){
+      winkapi.getDevices(function(){
+        done();
+      });
+    });
+  });
+
+  it('get icons', function(done){
+    setup(function(err, winkapi){
+      winkapi.getIcons(function(){
+        done();
+      });
+    });
+  });
+
+  it('get channels', function(done){
+    setup(function(err, winkapi){
+      winkapi.getChannels(function(){
+        done();
+      });
+    });
+  });
+
+  it('get dial templates', function(done){
+    setup(function(err, winkapi){
+      winkapi.getDialTemplates(function(){
+        done();
+      });
+    });
+  });
+
+  it('get services', function(done){
+    setup(function(err, winkapi){
+      winkapi.getServices(function(){
+        done();
+      });
+    });
+  });
+
+  it('get scenes', function(done){
+    setup(function(err, winkapi){
+      winkapi.getScenes(function(){
+        done();
+      });
+    });
+  });
+
+  it('get robots', function(done){
+    setup(function(err, winkapi){
+      winkapi.getRobots(function(){
+        done();
+      });
+    });
+  });
+
 });
--- winkapi.js
+++ winkapi.js
@@ -2,35 +2,47 @@
 //   cf., http://docs.wink.apiary.io
 
 
-var https       = require('https')
-  , events      = require('events')
-  , url         = require('url')
-  , util        = require('util')
-  ;
+var https       = require('https');
+var events      = require('events');
+var url         = require('url');
+var util        = require('util');
+
+const DEFAULT_LOG_CALL = function(msg, props) {
+  console.log(msg);
+  if (!!props){
+    console.log(props);
+  }
+};
+
+var DEFAULT_LOGGER = {
+  error   : DEFAULT_LOG_CALL,
+  warning : DEFAULT_LOG_CALL,
+  notice  : DEFAULT_LOG_CALL,
+  info    : DEFAULT_LOG_CALL,
+  debug   : DEFAULT_LOG_CALL
+};
 
 
-var DEFAULT_LOGGER = { error   : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
-                     , warning : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
-                     , notice  : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
-                     , info    : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
-                     , debug   : function(msg, props) { console.log(msg); if (!!props) console.log(props);             }
-                     };
-
+function setupLogger(self) {
+  var k;
+  for (k in DEFAULT_LOGGER) {
+    if ((DEFAULT_LOGGER.hasOwnProperty(k)) && (typeof self.logger[k] === 'undefined')) {
+      self.logger[k] = DEFAULT_LOGGER[k];
+    }
+  }
+}
 
 var WinkAPI = function(options) {
-  var k;
-
   var self = this;
 
-  if (!(self instanceof WinkAPI)) return new WinkAPI(options);
+  if (!(self instanceof WinkAPI)) {
+    return new WinkAPI(options);
+  }
 
   self.options = options;
 
-  self.logger = self.options.logger  || {};
-  for (k in DEFAULT_LOGGER) {
-    if ((DEFAULT_LOGGER.hasOwnProperty(k)) && (typeof self.logger[k] === 'undefined'))  self.logger[k] = DEFAULT_LOGGER[k];
-  }
-
+  self.logger = self.options.logger  || DEFAULT_LOGGER;
+  setupLogger(self);
   self.oauth = {};
 };
 util.inherits(WinkAPI, events.EventEmitter);
@@ -41,27 +53,47 @@ WinkAPI.prototype.login = function(username, passphrase, callback) {
 
   var self = this;
 
-  if (typeof callback !== 'function') throw new Error('callback is mandatory for login');
+  if (typeof callback !== 'function') {
+    throw new Error('callback is mandatory for login');
+  }
 
-  json = { username      : username
-         , client_secret : self.options.clientSecret
-         , password      : passphrase
-         , client_id     : self.options.clientID
-         , grant_type    : 'password'
-         };
+  json = {
+    username      : username,
+    client_secret : self.options.clientSecret,
+    password      : passphrase,
+    client_id     : self.options.clientID,
+    grant_type    : 'password'
+  };
+
   self.invoke('POST', '/oauth2/token', json, function(err, code, results) {
-    var data, errors;
 
-    if (!!err) return callback(err);
 
-    data = results.data;
-    errors = (!!results.errors) && util.isArray(results.errors) && (results.errors.length > 0) && results.errors;
-    if ((!!errors) || (!data)) {
-      return callback(new Error('invalid credentials: ' + JSON.stringify(!!errors ? errors : results)));
-    }
+   try {
+     var data, errors;
 
-    self.oauth = results.data;
-    callback(null);
+     if (!!err) {
+       return callback(err, self);
+     }
+
+     if (!results) {
+       return callback(new Error('No Results from Login'), self);
+     }
+
+     data = results.data;
+     errors = (!!results.errors) && util.isArray(results.errors) && (results.errors.length > 0) && results.errors;
+
+     if ((!!errors) || (!data)) {
+       var error = new Error('invalid credentials: ' + JSON.stringify(!!errors ? errors : results));
+       return callback(error, self);
+     }
+
+     self.oauth = results.data;
+     callback(null, self);
+   }
+     catch(e){
+       console.log('', e);
+     }
+
   });
 
   return self;
@@ -72,13 +104,16 @@ WinkAPI.prototype._refresh = function(callback) {
 
   var self = this;
 
-  if (typeof callback !== 'function') throw new Error('callback is mandatory for refresh');
+  if (typeof callback !== 'function') {
+    throw new Error('callback is mandatory for refresh');
+  }
 
-  json = { client_id     : self.options.clientID
-         , client_secret : self.options.clientSecret
-         , refresh_token : self.oauth.refresh_token
-         , grant_type    : 'refresh_token'
-         };
+  json = {
+    client_id     : self.options.clientID,
+    client_secret : self.options.clientSecret,
+    refresh_token : self.oauth.refresh_token,
+    grant_type    : 'refresh_token'
+  };
   self.invoke('POST', '/oauth2/token', json, function(err, code, results) {
     var data, errors;
 
@@ -102,6 +137,14 @@ WinkAPI.prototype.getUser = function(callback) {
   return this.roundtrip('GET', '/users/me', callback);
 };
 
+WinkAPI.prototype.getScenes = function(callback){
+  return this.roundtrip('GET','/users/me/scenes', callback);
+};
+
+WinkAPI.prototype.getRobots = function(callback){
+  return this.roundtrip('GET','/users/me/robots', callback);
+};
+
 WinkAPI.prototype.setUser = function(props, callback) {
   return this.roundtrip('PUT', '/users/me', props, callback);
 };
@@ -112,7 +155,9 @@ WinkAPI.prototype.getDevices = function(callback) {
   return self.invoke('GET', '/users/me/wink_devices', function(err, code, results) {
     var data, datum, devices, errors, i, k;
 
-    if (!!err) return callback(err);
+    if (!!err) {
+      return callback(err);
+    }
 
     data = results.data;
     errors = (!!results.errors) && util.isArray(results.errors) && (results.errors.length > 0) && results.errors;
@@ -125,15 +170,7 @@ WinkAPI.prototype.getDevices = function(callback) {
       datum = data[i];
 
       for (k in datum) if ((datum.hasOwnProperty(k)) && (k.indexOf('_id') === (k.length - 3))) {
-        devices.push({ id      : datum[k]
-                     , type    : k.slice(0, -3)
-                     , name    : datum.name
-                     , path    : '/' + k.slice(0, -3) + 's/' + datum[k]
-                     , dials   : self._children(datum, 'dials')
-                     , outlets : self._children(datum, 'outlets')
-                     , props   : datum
-                     });
-        break;
+        devices.push(devicePayload(datum, k ,self));
       }
     }
 
@@ -141,23 +178,32 @@ WinkAPI.prototype.getDevices = function(callback) {
   });
 };
 
+function devicePayload(datum, k, self) {
+  return {
+    id: datum[k],
+    type: k.slice(0, -3),
+    name: datum.name,
+    path: '/' + k.slice(0, -3) + ((k.slice(-5, -3)=='ch')?'es/':'s/') + datum[k],
+    dials: self._children(datum, 'dials'),
+    outlets: self._children(datum, 'outlets'),
+    props: datum
+  };
+}
+
 WinkAPI.prototype.getDevice = function(device, callback) {
   var self = this;
 
   return self.roundtrip('GET', device.path, function(err, datum) {
     var k;
 
-    if (!!err) return callback(err);
+    if (!!err) {
+      return callback(err);
+    }
 
-    for (k in datum) if ((datum.hasOwnProperty(k)) && (k.indexOf('_id') === (k.length - 3))) {
-      return callback(null, { id      : datum[k]
-                            , type    : k.slice(0, -3)
-                            , name    : datum.name
-                            , path    : '/' + k.slice(0, -3) + 's/' + datum[k]
-                            , dials   : self._children(datum,  'dials')
-                            , outlets : self._children(datum, 'outlets')
-                            , props   : datum
-                            });
+    for (k in datum){
+      if ((datum.hasOwnProperty(k)) && (k.indexOf('_id') === (k.length - 3))) {
+        return callback(null, devicePayload(datum, k, self));
+      }
     }
 
     callback(null, null);
@@ -167,7 +213,9 @@ WinkAPI.prototype.getDevice = function(device, callback) {
 WinkAPI.prototype._children = function(datum, prefix) {
   var child, children, i, id, type;
 
-  if (!datum[prefix]) return null;
+  if (!datum[prefix]) {
+    return null;
+  }
 
   children = {};
 
@@ -175,14 +223,15 @@ WinkAPI.prototype._children = function(datum, prefix) {
   id = type + '_id';
   for (i = 0; i < datum[prefix].length; i++) {
     child = datum[prefix][i];
-    if (!child[id]) continue;
-
-    children[child[id]] = { id    : child[id]
-                          , type  : type
-                          , name  : child.name
-                          , path  : '/' + prefix + '/' + child[id]
-                          , props : child
-                          };
+    if (child[id]) {
+      children[child[id]] = {
+        id: child[id],
+        type: type,
+        name: child.name,
+        path: '/' + prefix + '/' + child[id],
+        props: child
+      };
+    }
   }
 
   return children;
@@ -192,30 +241,34 @@ WinkAPI.prototype.setDevice = function(device, props, callback) {
   var self = this;
 
   return self.roundtrip('PUT', device.path, props, function(err, datum) {
-    if (!!err) return callback(err);
+    if (!!err) {
+      return callback(err);
+    }
 
-    return callback(null, { id      : device.id
-                          , type    : device.type
-                          , name    : device.name
-                          , path    : device.path
-                          , dials   : self._children(datum,  'dials')
-                          , outlets : self._children(datum, 'outlets')
-                          , props   : datum
-                          });
+    return callback(null, {
+      id      : device.id,
+      type    : device.type,
+      name    : device.name,
+      path    : device.path,
+      dials   : self._children(datum,  'dials'),
+      outlets : self._children(datum, 'outlets'),
+      props   : datum
+    });
   });
 };
+
 WinkAPI.prototype.setOutlet = WinkAPI.prototype.setDevice;
 
 WinkAPI.prototype.getIcons = function(callback) {
-  return this.roundtrip('GET', '/icons', callback);
+  return this.roundtrip('GET', '/icons',{}, callback);
 };
 
 WinkAPI.prototype.getChannels = function(callback) {
-  return this.roundtrip('GET', '/channels', callback);
+  return this.roundtrip('GET', '/channels',{}, callback);
 };
 
 WinkAPI.prototype.getServices = function(callback) {
-  return this.roundtrip('GET', '/users/me/linked_services', callback);
+  return this.roundtrip('GET', '/users/me/linked_services',{}, callback);
 };
 
 WinkAPI.prototype.newService = function(props, callback) {
@@ -223,7 +276,7 @@ WinkAPI.prototype.newService = function(props, callback) {
 };
 
 WinkAPI.prototype.getTrigger = function(id, callback) {
-  return this.roundtrip('GET', '/triggers/' + id, callback);
+  return this.roundtrip('GET', '/triggers/' + id,{}, callback);
 };
 
 WinkAPI.prototype.setTrigger = function(id, props, callback) {
@@ -231,7 +284,7 @@ WinkAPI.prototype.setTrigger = function(id, props, callback) {
 };
 
 WinkAPI.prototype.getDialTemplates = function(callback) {
-  return this.roundtrip('GET', '/dial_templates', callback);
+  return this.roundtrip('GET', '/dial_templates',{}, callback);
 };
 
 WinkAPI.prototype.setDialValue = function(dial, value, callback) {
@@ -239,7 +292,13 @@ WinkAPI.prototype.setDialValue = function(dial, value, callback) {
 };
 
 WinkAPI.prototype.setDialLabels = function(dial, mainLabel, secondaryLabel, callback) {
-  return this.setDial(dial, { label: mainLabel, labels: [ mainLabel, secondaryLabel] }, callback);
+  return this.setDial(dial, {
+    label: mainLabel,
+    labels: [
+      mainLabel,
+      secondaryLabel
+    ]
+  }, callback);
 };
 
 WinkAPI.prototype.setDial = function(dial, props, callback) {
@@ -247,24 +306,26 @@ WinkAPI.prototype.setDial = function(dial, props, callback) {
 
   var self = this;
 
-  json = { name                  : props.name                  || dial.props.name
-         , label                 : props.label                 || dial.props.label
-         , labels                : props.labels                || dial.props.labels
-         , position              : props.position              || dial.props.position
-         , value                 : props.value                 || dial.props.value
-         , brightness            : props.brightness            || dial.props.brightness
-         , channel_configuration : props.channel_configuration || dial.props.channel_configuration 
-         , dial_configuration    : props.dial_configuration    || dial.props.dial_configuration
-         };
+  json = {
+    name                  : props.name                  || dial.props.name,
+    value                 : props.value                 || dial.props.value,
+    label                 : props.label                 || dial.props.label,
+    labels                : props.labels                || dial.props.labels,
+    position              : props.position              || dial.props.position,
+    brightness            : props.brightness            || dial.props.brightness,
+    channel_configuration : props.channel_configuration || dial.props.channel_configuration,
+    dial_configuration    : props.dial_configuration    || dial.props.dial_configuration
+  };
   return self.roundtrip('PUT', dial.path, json, function(err, datum) {
     if (!!err) return callback(err);
 
-    return callback(null, { id      : dial.id
-                          , type    : dial.type
-                          , name    : dial.name
-                          , path    : dial.path
-                          , props   : datum
-                          });
+    return callback(null, {
+      id      : dial.id,
+      type    : dial.type,
+      name    : dial.name,
+      path    : dial.path,
+      props   : datum
+    });
   });
 };
 
@@ -280,7 +341,9 @@ WinkAPI.prototype.roundtrip = function(method, path, json, callback) {
   return self.invoke(method, path, json, function(err, code, results) {
     var data, errors;
 
-    if (!!err) return callback(err);
+    if (!!err) {
+      return callback(err);
+    }
 
     data = results.data;
     errors = (!!results.errors) && util.isArray(results.errors) && (results.errors.length > 0) && results.errors;
@@ -291,6 +354,15 @@ WinkAPI.prototype.roundtrip = function(method, path, json, callback) {
     callback(null, data);
   });
 };
+
+function analyzeStatusCode(method, statusCode) {
+  return {
+      GET: [200],
+      PUT: [200],
+      POST: [200, 201, 202],
+      DELETE: [200]
+    }[method].indexOf(statusCode) === -1;
+}
 
 WinkAPI.prototype.invoke = function(method, path, json, callback) {
   var options;
@@ -303,7 +375,12 @@ WinkAPI.prototype.invoke = function(method, path, json, callback) {
   }
   if (!callback) {
     callback = function(err, results) {
-      if (!!err) self.logger.error('invoke', { exception: err }); else self.logger.info(path, { results: results });
+      if (!!err) {
+        self.logger.error('invoke', { exception: err });
+      }
+      else {
+        self.logger.info(path, { results: results });
+      }
     };
   }
 
@@ -326,25 +403,33 @@ WinkAPI.prototype.invoke = function(method, path, json, callback) {
     response.on('data', function(chunk) {
       body += chunk.toString();
     }).on('end', function() {
-      var expected = { GET    : [ 200 ]
-                     , PUT    : [ 200 ]
-                     , POST   : [ 200, 201, 202 ]
-                     , DELETE : [ 200 ]
-                     }[method];
+      var statusCode = response.statusCode;
+      var badErrorCode = analyzeStatusCode(method, statusCode);
 
       var results = {};
 
-      try { results = JSON.parse(body); } catch(ex) {
-        self.logger.error(path, { event: 'json', diagnostic: ex.message, body: body });
-        return callback(ex, response.statusCode);
+      try {
+        results = JSON.parse(body);
+      }
+      catch(ex) {
+        self.logger.error(path, {
+          event: 'json',
+          diagnostic: ex.message,
+          body: body
+        });
+        return callback(ex, statusCode);
       }
 
-      if (expected.indexOf(response.statusCode) === -1) {
-         self.logger.error(path, { event: 'https', code: response.statusCode, body: body });
-         return callback(new Error('HTTP response ' + response.statusCode), response.statusCode, results);
+      if (badErrorCode) {
+        self.logger.error(path, {
+          event: 'https',
+          code: statusCode,
+          body: body
+        });
+        return callback(new Error('HTTP response ' + statusCode), statusCode, results);
       }
 
-      callback(null, response.statusCode, results);
+      callback(null, statusCode, results);
     }).on('close', function() {
       callback(new Error('premature end-of-file'));
     }).setEncoding('utf8');
@@ -356,4 +441,4 @@ WinkAPI.prototype.invoke = function(method, path, json, callback) {
 };
 
 
-exports.WinkAPI = WinkAPI;
+module.exports.WinkAPI = WinkAPI;
